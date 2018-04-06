751ad8f73a2ec0572856b268c31203b969ec18d4
Flarp <flurpdadurp@gmail.com>
2018-04-06 15:54:31 -0400

    strawberry fields, nothing is real

---
+++ midesque_to_mid.py
@@ -0,0 +1,3 @@
+from midiutil import MIDIFile
+
+file = MIDIFile(1, eventtime_is_ticks=True)
---
+++ time_manip_marker.lua
@@ -0,0 +1,25 @@
+---- marker_time_manip.lua
+-- time stretching markers
+-- ie markers at t = 14 32 56 with a factor of .5
+-- will be moved to t = 7 16 28
+ardour {
+	["type"] = "EditorAction",
+	name = "Change Speed of Markers"
+}
+
+function factory (_nada)
+	return function()
+		local v = Session:locations():list()
+        local dialog_contents = {
+            { type = "heading", title = "Change Marker Speed" },
+            { type = "number", key = "factor", title = "Stretch factor", min = 0, max = 1000, step = 0.001, digits = 20, default = 1 },
+        }
+        local dialog_gui = LuaDialog.Dialog("Change Marker Speed", dialog_contents)
+        local results = dialog_gui:run()
+		for location in v:iter() do
+			if location:is_mark() then
+				location:move_to(math.floor(location:start() * results["factor"]), 0)
+			end
+		end
+	end
+end
